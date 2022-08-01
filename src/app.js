ac4834a32552edbfe68f0c426682931b36a29a02
Sandeep <sandeepsoni@excellencetechnologies.in>
2022-08-01 16:37:43 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+// const mongoose = require('mongoose')
+//  mongoose.connect("mongodb://localhost:27017/prodatabase").then(()=>{console.log("connected succesfully")}).catch(()=>{console.log("err")})
+
+
+
+//  const playlistSchema =  new mongoose.Schema(
+// {
+//     firstname : String,
+//     email : String ,
+//     lastname: String ,
+//     password: String,
+// }
+//  )
+
+// const schema2 = new mongoose.Schema({
+    
+//     mobile_no: String,
+//     DOB : String
+// })
+// const Playlist1 = new mongoose.model("user",playlistSchema) 
+
+//  const Playlist = new mongoose.model("usersProfile",schema2)
+
+//  const recent = new Playlist({
+//     firstname : "sunny",
+//     email : "sunnyyadav988@gmail.com",
+//     lastname: "yadav",
+//     password: "sunnny@123",
+//  })
+
+// const recent2 = new Playlist({
+    
+//     mobile_no:"9888888888" ,
+//     DOB : "22/4/1996"
+// })
+// recent.save()
+//  recent2.save()
