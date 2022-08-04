e8904f84af6664b81e21901a44c0b0c2d10fd5ab
Sandeep <sandeepsoni@excellencetechnologies.in>
2022-08-04 12:36:18 +0530

    first commit

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,31 +1,37 @@
 require("../db/conn");
-const Regis = require("../models/scm")
-const bcrypt = require("bcrypt")
+const Regis = require("../models/scm");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const secretkey = "sunnyyadav988"
 
 
-exports.loginControl = async (req,res)=>{
+
+exports.loginControl = async (req, res) => {
+  try {
     
-    
-try{
-    const data = await Regis.findOne({username: req.body.username}) 
-    
+    const name = req.body.username;
     const bdpassword = req.body.Password;
-   const compare  = await bcrypt.compare(bdpassword,data.Password) 
-   console.log(compare)
-   if(compare){
-    res.status(200).send("ok")
-   }else{
-    res.status(400).send("error")
-   }
+    const data = await Regis.findOne({ username: name });
 
-    
-
-}
-catch{
-    res.status(400).send("error")
-}
-    
-    
-   
-
-}
+    const compare = await bcrypt.compare(bdpassword, data.Password);
+    // console.log(compare);
+    if (compare) {
+      try{
+        const token = jwt.sign({ name:name },process.env.secretkey, {
+            algorithm: "HS256",
+            expiresIn: 300,
+          })
+          res.status(200).send("ok");
+          console.log("token:", token);
+      }catch{
+        res.status(500).send("error")
+      }
+      
+    } else {
+      res.status(400).send("error");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
